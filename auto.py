c8110ed2667e1c077b14521e19783d7fcae71fb1
Hoagn-2504 <hoangvu25042004@gmail.com>
2024-08-31 16:43:10 +0700

    update

--- auto.py
+++ auto.py
@@ -1,19 +1,27 @@
 import pyautogui as pg
 import time 
+
 time.sleep(2)
-pg.doubleClick((41, 734))
+pg.hotkey('win', 'r')
 time.sleep(1)
-pg.typewrite('https://web.telegram.org/a/')
+pg.write('chrome')
 time.sleep(1)
 pg.press('enter')
+time.sleep(1)
+pg.hotkey('alt', 'space')
+time.sleep(1)
+pg.press('x')
 time.sleep(2)
-pg.leftClick(250,270)
+pg.write('https://web.telegram.org/a/')
+pg.press('enter')
+time.sleep(2)
+pg.leftClick(160,210)
 time.sleep(2)
 pg.leftClick(1070, 840)
 time.sleep(4)
 while True:
-    for _ in range(1000):
-        pos = pg.position(950, 700)    
+    for _ in range(2000):
+        pos = pg.position(700, 600)    
         pg.doubleClick(pos)
         cr = pg.position()
         if cr != pos:
