dcd62e1346f3d34a7a3e04dd2d8cdbc4b65b30ee
Hoagn-2504 <hoangvu25042004@gmail.com>
2024-08-31 16:53:08 +0700

    update

--- auto.py
+++ auto.py
@@ -1,6 +1,7 @@
 import pyautogui as pg
 import time 
 
+#Mở Chrome
 time.sleep(2)
 pg.hotkey('win', 'r')
 time.sleep(1)
@@ -8,24 +9,33 @@ pg.write('chrome')
 time.sleep(1)
 pg.press('enter')
 time.sleep(1)
+
+#Chỉnh kích thước sang toàn màn hình nếu chưa full
 pg.hotkey('alt', 'space')
 time.sleep(1)
 pg.press('x')
 time.sleep(2)
-pg.write('https://web.telegram.org/a/')
+
+#Đi đến trang Telegram
+pg.write('https://web.telegram.org')
+time.sleep(1)
 pg.press('enter')
 time.sleep(2)
+
+#Mở berasig (app Berasig được ghim lên đầu)
 pg.leftClick(160,210)
 time.sleep(2)
 pg.leftClick(1070, 830)
 time.sleep(4)
 while True:
+    #Auto click
     for _ in range(2000):
         pos = pg.position(700, 600)    
         pg.doubleClick(pos)
         cr = pg.position()
         if cr != pos:
             break
+    #Nhấn vào Done
     time.sleep(5)
     pos = pg.position(950, 780)
     pg.leftClick(pos)
